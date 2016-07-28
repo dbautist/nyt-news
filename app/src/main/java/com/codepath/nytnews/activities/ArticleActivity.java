6708ec267c9a9d7992a07ade23a88135b710824e
Dianne Bautista <dianne.bautista@tapjoy.com>
2016-07-28 17:54:44 -0400

    implement up button; allow for sharing webview's link

--- app/src/main/java/com/codepath/nytnews/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/nytnews/activities/ArticleActivity.java
@@ -1,7 +1,13 @@
 package com.codepath.nytnews.activities;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -24,6 +30,8 @@ public class ArticleActivity extends AppCompatActivity {
     setContentView(R.layout.activity_article);
     ButterKnife.bind(this);
 
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     Article article = Parcels.unwrap(getIntent().getParcelableExtra(AppConstants.ARTICLE_EXTRA));
     if (article != null) {
       loadArticle(article);
@@ -41,5 +49,22 @@ public class ArticleActivity extends AppCompatActivity {
       }
     });
     articleWebView.loadUrl(article.webUrl);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_article, menu);
+
+    MenuItem item = menu.findItem(R.id.menu_item_share);
+    ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+    shareIntent.setType("text/plain");
+
+    // pass in the URL currently being used by the WebView
+    shareIntent.putExtra(Intent.EXTRA_TEXT, articleWebView.getUrl());
+
+    miShare.setShareIntent(shareIntent);
+    return super.onCreateOptionsMenu(menu);
   }
 }
